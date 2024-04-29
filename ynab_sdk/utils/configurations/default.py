4ba02bafa8f4be379af69866b19156b8e813095c
Ga68 <Ga68@GitHub.com>
2024-04-29 13:53:39 -0700

    Ensure code is inline with upstream main --- Revert "Update default API host" This reverts commit 0da6ee824784621fdead25c089122fed91254d1e.

--- ynab_sdk/utils/configurations/default.py
+++ ynab_sdk/utils/configurations/default.py
@@ -3,7 +3,7 @@ import logging
 
 class DefaultConfig:
     def __init__(
-        self, api_key: str, base_path="/v1", host="https://api.ynab.com"
+        self, api_key: str, base_path="/v1", host="https://api.youneedabudget.com"
     ):
         self.api_key = api_key
         self.base_path = base_path
